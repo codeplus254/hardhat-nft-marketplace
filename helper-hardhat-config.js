19dda2edee585818175d6f9582c4c746e6556298
codeplus254 <ronnymageh@gmail.com>
2025-02-23 15:12:17 +0300

    allows listing nft, updating nft, cancelling nft, buying nft and withdrawing proceeds from marketplace

---
+++ helper-hardhat-config.js
@@ -0,0 +1,44 @@
+const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0x3fd2fec10d06ee8f65e7f2e95f5c56511359ece3f33960ad8a866ae24a8ff10b", // 500 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    11155111: {
+        name: "sepolia",
+        subscriptionId: "588",
+        gasLane: "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae", // 500 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile = "../nextjs-nft-marketplace-moralis-fcc/constants/networkMapping.json"
+const frontEndContractsFile2 =
+    "../nextjs-nft-marketplace-thegraph-fcc/constants/networkMapping.json"
+const frontEndAbiLocation = "../nextjs-nft-marketplace-moralis-fcc/constants/"
+const frontEndAbiLocation2 = "../nextjs-nft-marketplace-thegraph-fcc/constants/"
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsFile,
+    frontEndContractsFile2,
+    frontEndAbiLocation,
+    frontEndAbiLocation2,
+}
